c372046b12a0efa3fafa68b5b1d00ae80cef0363
Vineeth Pothu <vineeth.pothu@oyorooms.com>
2021-03-19 21:26:06 +0530

    merge branches

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -7,23 +7,17 @@
         <div class="flex justify-between">
 
             <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-2">
-                {{ __('Property Overview Demo') }}
+                {{ __('Property Details') }}
             </h2>
 
-            <div>
-                <a href="/api/owner" >
-                    <x-button >Home owner</x-button>
-                </a>
-                <a href="/3dmodel">
-                    <x-button>3d Overview</x-button>
-                </a>
+            <a href="/api/owner" >
+                <x-button >Home owner</x-button>
+            </a>
 
-            </div>
+
 
         </div>
     </x-slot>
-
-
 
 
     <div class="py-12">
@@ -33,15 +27,75 @@
             <div class="mx-w-full bg-white overflow-hidden shadow-sm sm:rounded-lg m-2 cursor-pointer">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-start">
-                        <script src="https://aframe.io/releases/0.7.1/aframe.min.js"></script>
+                        <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+                        <script src="https://unpkg.com/aframe-template-component@3.x.x/dist/aframe-template-component.min.js"></script>
+                        <script src="https://unpkg.com/aframe-layout-component@4.x.x/dist/aframe-layout-component.min.js"></script>
+                        <script src="https://unpkg.com/aframe-event-set-component@5.x.x/dist/aframe-event-set-component.min.js"></script>
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                        <script src="libraries/embed360.js"></script>
 
 
-                        <div id="myEmbeddedScene">
-                            <a-scene>
 
-                                <a-camera></a-camera>
-                                <a-sky src="{{ asset('uploads/1616155060.jpeg') }}"></a-sky>
-                            </a-scene>
+                        {{--                        <div id="myEmbeddedScene">--}}
+                        {{--                            <a-scene>--}}
+
+                        {{--                                <a-camera></a-camera>--}}
+                        {{--                                <a-sky src="{{ asset('uploads/name.jpeg') }}"></a-sky>--}}
+                        {{--                            </a-scene>--}}
+                        {{--                        </div>--}}
+                        @php
+                            $path = public_path('uploads');
+                            $files = File::allFiles($path);
+
+
+
+                        @endphp
+
+                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            </ol>
+
+                            <div class="carousel-item active">
+                                <div class="embed360">
+                                    <img src="{{ asset('uploads/name.jpeg') }}">
+                                </div>
+
+                                <script>
+                                    embed360('.embed360');
+                                </script>
+
+                            </div>
+
+                            <div class="carousel-inner">
+                                @foreach ($files as $key)
+                                    <div class="carousel-item">
+                                        <div class="embed360">
+                                            <img src="{{ asset('uploads/'.$key->getRelativePathname()) }}">
+                                        </div>
+
+                                        <script>
+                                            embed360('.embed360');
+                                        </script>
+
+                                    </div>
+
+                                @endforeach
+
+                            </div>
+
+                            <a class="carousel-control-prev" href="#myCarousel" role="button"  data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true">   </span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+
                         </div>
 
 
@@ -76,6 +130,22 @@
             </div>
 
 
+            <div class="py-12">
+                <p class="ml-5 text-3xl">3D Floor Plan</p>
+                <div class="mx-w-full mx-auto sm:px-6 lg:px-8">
+
+                    <div class="mx-w-full bg-white overflow-hidden shadow-sm sm:rounded-lg m-2 cursor-pointer">
+                        <div class="p-6 bg-white border-b border-gray-200">
+
+                            <model-viewer src="{{ asset('3d/appartamento3.gltf') }}" camera-controls auto-rotate></model-viewer>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
 
 
 </x-app-layout>
--- resources/views/owner.blade.php
+++ resources/views/owner.blade.php
@@ -9,11 +9,8 @@
             </h2>
 
             <div>
-                <a href="/" >
+                <a href="/guest" >
                     <x-button >Guest</x-button>
-                </a>
-                <a>
-                    <x-button>3d Overview</x-button>
                 </a>
 
             </div>
@@ -69,6 +66,50 @@
 
     <div class="py-12">
         <p class="ml-5 text-2xl">Upload your 2D floor plan here for guests to view </p>
+    </div>
+
+    <div class="container">
+
+        <div class="panel panel-primary">
+            <div class="panel-heading"><h2>Floor plan sketch upload</h2></div>
+            <div class="panel-body">
+
+                @if ($message = \Illuminate\Support\Facades\Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    <img src="uploads/{{ \Illuminate\Support\Facades\Session::get('file') }}">
+                @endif
+
+                    <input type="radio" id="2d-b/w" name="plan" value="2d-b/w" checked>
+                    <label for="2d-b/w">2d-b/w</label><br>
+                    <input type="radio" id="2d-color" name="plan" value="2d-color">
+                    <label for="2d-color">2d-color</label><br>
+                    <input type="radio" id="2d-premium" name="plan" value="2d-premium">
+                    <label for="2d-premium">2d-premium</label><br>
+                    <input type="radio" id="3d" name="plan" value="3d">
+                    <label for="3d">3d</label><br>
+
+                <form action="/guest" method="GET" enctype="multipart/form-data">
+                    @csrf
+
+
+                    <div class="row">
+
+                        <div class="col-md-6">
+                            <input type="file" name="file" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <button type="submit" class="btn btn-success">Upload</button>
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </div>
 </x-app-layout>
 
